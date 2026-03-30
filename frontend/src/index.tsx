df71dc8975462b20a1b7e89f4e8d24932dee9b6b
Landon Lego <legolandon25@gmail.com>
2026-03-30 13:43:13 +0000

    dark theme default

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,7 @@ import App from './pages/App.tsx'
 
 document.documentElement.setAttribute(
     "data-theme",
-    localStorage.getItem("theme") || "light"
+    localStorage.getItem("theme") || "dark"
 );
 
 createRoot(document.getElementById('root')!).render(
