bdfeb19e8ea7d88eb78d7b3e8cffbc50b4bfc367
LegoLandon7 <legolandon25@gmail.com>
2026-03-30 15:52:08 -0400

    added all my socials

--- frontend/src/pages/Socials.tsx
+++ frontend/src/pages/Socials.tsx
@@ -14,15 +14,15 @@ export default function Socials() {
 
             <SocialButtonWrapper 
                 socials={[
-                    { social: 'discord', link: 'https://discord.gg/', username: 'Discord Server - llegonetwork' },
+                    { social: 'discord', link: 'https://discord.gg/ghHCxWxDMG', username: 'Discord Server - llegonetwork' },
                     { social: 'email', link: 'mailto:contact@llegonetwork.dev', username: 'Email - llegonetwork' },
                     { social: 'github', link: 'https://github.com/LegoLandon7', username: 'LegoLandon7' },
-                    { social: 'discord', link: 'https://discord.gg/', username: 'legomaster_01' },
+                    { social: 'discord', link: 'https://discord.com/users/623964733799923733', username: 'legomaster_01' },
                     { social: 'youtube', link: 'https://youtube.com/@legomaster_01', username: 'legomaster_01' },
                     { social: 'reddit', link: 'https://reddit.com/user/legomaster_01', username: 'legomaster_01' },
                     { social: 'tiktok', link: 'https://tiktok.com/@legomaster_00', username: 'legomaster_00' },
                     { social: 'tiktok', link: 'https://tiktok.com/@thelegoguy_0', username: 'thelegoguy_0' },
-                    { social: 'instagram', link: 'https://instagram.com/thelegoguy_0', username: 'thelegoguy_0' },
+                    { social: 'instagram', link: 'https://instagram.com/lego.guy0', username: 'thelegoguy_0' },
                 ]}
             />
         </>
